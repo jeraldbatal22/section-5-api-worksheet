395393b9daf10ffa9ee4aacbf3d9ca9807a07956
Jerald Batal <jebatal@highlysucceed.com>
2025-12-16 11:08:58 +0800

    Commented out connect To database since were using supabase now

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import errorMiddleware from "./middleware/error-handler.middleware.ts";
-import connectToDatabase from "./database/ps.ts";
+// import connectToDatabase from "./database/ps.ts";
 import router from "./routes/index.routes.ts";
 import createRateLimiter from "./middleware/rate-limiter.middleware.ts";
 import { PORT } from "./config/env.ts";
@@ -19,7 +19,7 @@ app.use(errorMiddleware);
 
 if (process.env.NODE_ENV !== "test") {
   app.listen(PORT as any, "0.0.0.0", async () => {
-    await connectToDatabase();
+    // await connectToDatabase();
     console.log(`Server running at http://localhost:${PORT}`);
   });
 }
